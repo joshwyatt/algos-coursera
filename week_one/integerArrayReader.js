f125bddae301ecf14cb297feba07e44bea2259ed
joshWyatt <joshuawyatt123@gmail.com>
2014-10-19 11:10:46 -0700

    Remove extra newline character from readfile

--- week_one/integerArrayReader.js
+++ week_one/integerArrayReader.js
@@ -7,9 +7,11 @@
   fs.readFile(file, function(err, data){
     if( err ) throw err;
     var array = data.toString().split('\n');
+    array.pop();
 
     var result = countInversions(array);
-    console.log(result);
+    console.log('The number of inversions for the array made out of %s is: %d', file, result);
+    // console.log(result);
   });
 
 })();
