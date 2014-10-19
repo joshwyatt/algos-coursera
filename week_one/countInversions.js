94293d62dd9d2e70c0fd88086fd1ef119ea46cd5
joshWyatt <joshuawyatt123@gmail.com>
2014-10-18 19:11:43 -0700

    Add comments

--- week_one/countInversions.js
+++ week_one/countInversions.js
@@ -1,4 +1,6 @@
 ;(function(){
+  // This is an only slightly modified merge sort that will count the number of 
+  // inversions in the passed-in array.
   function mergeSortAndCountInversions(array){
 
     var numberOfInversions = 0;
@@ -10,10 +12,14 @@
       var halfWithLeftovers;
 
       while( leftIndex < leftHalf.length && rightIndex < rightHalf.length ){
+        // parseInt is used here because I am reading from a text file and need
+        // to sort numbers, not strings
         if( parseInt(leftHalf[leftIndex]) < parseInt(rightHalf[rightIndex]) ){
           results.push(leftHalf[leftIndex++]);
         }else{
           results.push(rightHalf[rightIndex++]);
+          // Any number on the right hand array has the number of inversions of
+          // how many digits are left in the left array
           numberOfInversions += leftHalf.length - leftIndex;
         }
       }
