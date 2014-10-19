12b69989366faa7649bcc6baf7802faaac94c5f7
joshWyatt <joshuawyatt123@gmail.com>
2014-10-19 10:50:15 -0700

    Add log

--- week_one/integerArrayReader.js
+++ week_one/integerArrayReader.js
@@ -7,6 +7,9 @@
   fs.readFile(file, function(err, data){
     if( err ) throw err;
     var array = data.toString().split('\n');
+
+    var result = countInversions(array);
+    console.log(result);
   });
 
 })();
