fb1e34e53e70618bd8ffd9a883435b19add087be
joshWyatt <joshuawyatt123@gmail.com>
2014-10-20 14:13:07 -0700

    Change file to be closestPointsRunner and begin writing actual closestPoints algo

--- week_one/closestPoints/closestPointsRunner.js
+++ week_one/closestPoints/closestPointsRunner.js
@@ -1,11 +1,13 @@
 ;(function(){
   var TwoDGraph = require('./TwoDGraph.js');
+  var closestPoints = require('./closestPoints.js');
 
   if( process.argv.length !== 7 ){
     console.log('Usage:');
     console.log('node closestPoints.js [xMin] [yMin] [xMax] [yMax] [numberOfPoints]');
   }
 
+  var points;
   var xMin = process.argv[2];
   var yMin = process.argv[3];
   var xMax = process.argv[4];
@@ -13,5 +15,7 @@
   var numberOfPoints = process.argv[6];
 
   var twoDGraph = new TwoDGraph(xMin, yMin, xMax, yMax, numberOfPoints);
-  console.log(twoDGraph.points);
+
+  points = twoDGraph.points;
+  console.log(points);
 })();
