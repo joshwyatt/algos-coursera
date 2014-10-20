3458c4d202ba513702a1c2e804a8fcb195cb06d5
joshWyatt <joshuawyatt123@gmail.com>
2014-10-20 13:29:18 -0700

    Require TwoDGraph into closestPoints and begin generate points

--- week_one/TwoDGraph.js
+++ week_one/TwoDGraph.js
@@ -1,13 +1,24 @@
 ;(function(){
 
   function TwoDGraph(xMin, yMin, xMax, yMax, numberOfPoints){
-    
+
     this.xMin = xMin;
     this.yMin = yMin;
     this.xMax = xMax;
     this.yMax = yMax;
     this.numberOfPoints = numberOfPoints;
+
   }
+
+  TwoDGraph.prototype.generatePoints = function(){
+    var points = [];
+
+    while( this.numberOfPoints-- ){
+      console.log(this.numberOfPoints);
+    }
+
+    return points;
+  };
 
   module.exports = TwoDGraph;
 
--- week_one/closestPoints.js
+++ week_one/closestPoints.js
@@ -1,3 +1,17 @@
 ;(function(){
-  
+  var TwoDGraph = require('./TwoDGraph.js');
+
+  if( process.argv.length !== 7 ){
+    console.log('Usage:');
+    console.log('node closestPoints.js [xMin] [yMin] [xMax] [yMax] [numberOfPoints]');
+  }
+
+  var xMin = process.argv[2];
+  var yMin = process.argv[3];
+  var xMax = process.argv[4];
+  var yMax = process.argv[5];
+  var numberOfPoints = process.argv[6];
+
+  var twoDGraph = new TwoDGraph(xMin, yMin, xMax, yMax, numberOfPoints);
+  twoDGraph.generatePoints();
 })();
