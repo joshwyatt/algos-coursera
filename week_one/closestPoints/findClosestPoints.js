66d359188493d537af6b19fd54092efad4b58291
joshWyatt <joshuawyatt123@gmail.com>
2014-10-20 15:38:46 -0700

    Write calculate distance for single points

--- week_one/closestPoints/findClosestPoints.js
+++ week_one/closestPoints/findClosestPoints.js
@@ -1,7 +1,7 @@
 ;(function(){
   function findClosestPoints(points){
     var closestPointsDetails = {
-      closestPoints: [undefined, undefined],
+      closestPoints: [],
       distance: undefined
     };
 
@@ -20,6 +20,8 @@
         closestDistance: 0
       };
 
+      //if single points, calculate distance between the two
+
       return results;
 
     }
@@ -28,7 +30,7 @@
         return {
           sortedByX: points,
           sortedByY: points,
-          closestPoints: undefined,
+          closestPoints: [],
           closestDistance: undefined
         };
       }
@@ -42,7 +44,13 @@
       processedLeftHalf = recursivelySubdivide(leftHalf);
       processedRightHalf = recursivelySubdivide(rightHalf);
 
-      // mergeSortAndFindClosest(processedLeftHalf, processedRightHalf);
+      mergeSortAndFindClosest(processedLeftHalf, processedRightHalf);
+    }
+
+    function calculateDistance(pointOne, pointTwo){
+      var distanceBetweenX = pointOne[0] - pointTwo[0];
+      var distanceBetweenY = pointOne[1] - pointTwo[1];
+      return Math.sqrt(Math.pow(distanceBetweenX, 2) + Math.pow(distanceBetweenY, 2));
     }
 
     recursivelySubdivide(points);
