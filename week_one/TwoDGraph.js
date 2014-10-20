4b9e0a789b6877d4c84b4e5d70f4492c37a6c35e
joshWyatt <joshuawyatt123@gmail.com>
2014-10-20 13:15:54 -0700

    Create TwoDGraph class

--- week_one/TwoDGraph.js
+++ week_one/TwoDGraph.js
@@ -1,3 +1,14 @@
 ;(function(){
-  
+
+  function TwoDGraph(xMin, yMin, xMax, yMax, numberOfPoints){
+    
+    this.xMin = xMin;
+    this.yMin = yMin;
+    this.xMax = xMax;
+    this.yMax = yMax;
+    this.numberOfPoints = numberOfPoints;
+  }
+
+  module.exports = TwoDGraph;
+
 })();
