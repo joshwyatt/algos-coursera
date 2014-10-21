1400dd90158f369e09391606497d69c3edd1cc29
joshWyatt <joshuawyatt123@gmail.com>
2014-10-20 17:36:02 -0700

    Write comments

--- week_one/closestPoints/findClosestPoints.js
+++ week_one/closestPoints/findClosestPoints.js
@@ -2,11 +2,17 @@
   var mergeSortByXorY = require('./mergeSortByXorY.js');
 
   function findClosestPoints(points){
+
+    // preprocess by sorting all points by x
     var pointsSortedByX = mergeSortByXorY(points, 'x');
     console.log(pointsSortedByX);
 
+    // preprocess by sorting all points by y
     var pointsSortedByY = mergeSortByXorY(points, 'y');
     console.log((pointsSortedByY));
+
+    // recursively split on x until down to base case where we have a single computation
+    // for distance
   }
 
   module.exports = findClosestPoints;
