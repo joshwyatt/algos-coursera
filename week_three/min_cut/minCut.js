32b2d06799cf8d17790dfe8b960c05113ade8964
joshWyatt <joshuawyatt123@gmail.com>
2014-11-05 13:43:59 -0800

    Complete minCut

--- week_three/min_cut/minCut.js
+++ week_three/min_cut/minCut.js
@@ -1,42 +1,55 @@
-var adjacencyList = {
-  1: [2, 3],
-  2: [1, 4],
-  3: [1, 4],
-  4: [2, 3]
-};
+function minCut(adjacencyList){
 
-var allEdges = [[1,2], [1,3], [2,1], [2,4], [3,1], [3,4], [4,2], [4,3]];
-var numberOfEdges = calculateNumberOfEdges(adjacencyList);
+  while( Object.keys(adjacencyList).length > 2 ){
+    var numberOfEdges = calculateNumberOfEdges(adjacencyList);
+    var edge = pickRandomEdge(adjacencyList, numberOfEdges);
 
-//randomly pick an edge
-function pickRandomEdge(adjacencyList){
-  var randomIndex = Math.floor(Math.random() * numberOfEdges);
-  var keys = Object.keys(adjacencyList);
-  var ticker = 0;
-  // debugger;
-  //while the current property length is greater than random index
-  while( randomIndex > adjacencyList[keys[ticker]].length - 1 ){
-    // decrement randomIndex and go to next property
-    randomIndex -= adjacencyList[keys[ticker++]].length;
+    collapseEdge(edge, adjacencyList);
   }
-  // return edge at index of randomIndex of this property
-  return [keys[ticker], adjacencyList[keys[ticker]][randomIndex]];
-}
 
-//how many edges are there.
-function calculateNumberOfEdges(adjacencyList){
-  var edges = 0;
-  for( var vertice in adjacencyList ){
-    edges += adjacencyList[vertice].length;
+  return adjacencyList[Object.keys(adjacencyList)[0]].length;
+
+  //randomly pick an edge
+  function pickRandomEdge(adjacencyList, numberOfEdges){
+    var randomIndex = Math.floor(Math.random() * numberOfEdges);
+    var keys = Object.keys(adjacencyList);
+    var ticker = 0;
+    // debugger;
+    //while the current property length is greater than random index
+    while( randomIndex > adjacencyList[keys[ticker]].length - 1 ){
+      // decrement randomIndex and go to next property
+      randomIndex -= adjacencyList[keys[ticker++]].length;
+    }
+    // return edge at index of randomIndex of this property
+    return [+keys[ticker], adjacencyList[keys[ticker]][randomIndex]];
   }
-  return edges;
-}
 
-//collapse edge
-function collapseEdge(edge, adjacencyList){
-  var verticeOne = edge[0];
-  var verticeTwo = edge[1];
-  //concat array at verticeTwo to array at verticeOne
-}
+  //how many edges are there.
+  function calculateNumberOfEdges(adjacencyList){
+    var edges = 0;
+    for( var vertex in adjacencyList ){
+      edges += adjacencyList[vertex].length;
+    }
+    return edges;
+  }
 
-show(pickRandomEdge(adjacencyList));
+  //collapse edge
+  function collapseEdge(edge, adjacencyList){
+    var vertexOne = edge[0];
+    var vertexTwo = edge[1];
+    //concat array at vertexTwo to array at vertexOne
+    adjacencyList[vertexOne] = adjacencyList[vertexOne].concat(adjacencyList[vertexTwo]);
+    //delete vertexTwo
+    delete adjacencyList[vertexTwo];
+    //iterate over edges at vertexOne and delete any ref to vertexOne or vertexTwo
+    adjacencyList[vertexOne] = adjacencyList[vertexOne].filter(function(vertex){
+      return !(vertex === vertexOne || vertex === vertexTwo);
+    });
+    //iterate over all arrays and remove any reference to vertexTwo
+    for(var vertexKey in adjacencyList){
+      adjacencyList[vertexKey] = adjacencyList[vertexKey].map(function(vertex){
+        return vertex === vertexTwo ? vertexOne : vertex;
+      });
+    }
+  }
+}
